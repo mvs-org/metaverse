9e2a072726c8496b606bebee40476cd974589a4d
sun <jiraiyav5@126.com>
2016-11-21 16:12:51 +0800

    fix warning in precomplie

--- include/bitcoin/consensus/libdevcore/Guards.h
+++ include/bitcoin/consensus/libdevcore/Guards.h
@@ -24,11 +24,11 @@
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
-#pragma warning(push)
+//#pragma warning(push)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <boost/thread.hpp>
-#pragma warning(pop)
+//#pragma warning(pop)
 #pragma GCC diagnostic pop
 
 namespace dev
