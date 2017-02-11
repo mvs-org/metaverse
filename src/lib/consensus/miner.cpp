7fcd30c3930d0f34391c9bee88fe70b26eb27d4a
sun <jiraiyav5@126.com>
2017-02-11 18:52:59 +0800

    add declare

--- src/lib/consensus/miner.cpp
+++ src/lib/consensus/miner.cpp
@@ -599,7 +599,7 @@ bool miner::put_result(const std::string& nonce, const std::string& mix_hash, co
 {
 	bool ret = false;
 	if(header_hash == "0x"+ to_string(HeaderAux::hashHead(new_block_->header))){
-		new_block_->header.nonce = (FixedHash<8>::Arith)h64(nonce);
+        auto s_nonce = "0x" + nonce;
         uint64_t n_nonce;
         if(sscanf(s_nonce.c_str(), "%lx", &n_nonce) != 1) {
 		    log::error(LOG_HEADER) << "nonce change error\n";
