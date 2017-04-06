9e5b773bdca583cb6396211f5c711c1eeeb7de85
betachen <betachen@yeah.net>
2017-04-06 13:32:18 +0300

    release v0.6.7

--- include/metaverse/bitcoin/version.hpp
+++ include/metaverse/bitcoin/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define MVS_VERSION "0.6.0"
+#define MVS_VERSION "0.6.7"
 #define MVS_MAJOR_VERSION 0
 #define MVS_MINOR_VERSION 6
-#define MVS_PATCH_VERSION 0
+#define MVS_PATCH_VERSION 7
 
 #endif
