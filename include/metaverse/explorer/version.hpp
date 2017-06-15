668fdd354850940f366f1c2cfae950458e2a29ca
betachen <betachen@yeah.net>
2017-06-16 02:55:36 +0800

    update 0.6.9

--- include/metaverse/explorer/version.hpp
+++ include/metaverse/explorer/version.hpp
@@ -12,7 +12,7 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define MVS_EXPLORER_VERSION "0.6.8"
+#define MVS_EXPLORER_VERSION "0.6.9"
 #define MVS_EXPLORER_MAJOR_VERSION 0
 #define MVS_EXPLORER_MINOR_VERSION 6
 #define MVS_EXPLORER_PATCH_VERSION 8
