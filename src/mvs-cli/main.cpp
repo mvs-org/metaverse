e4953da13ad52d18026d565cf52384ba304550b4
Jeremy Lan <air.petrichor@gmail.com>
2017-07-11 18:32:44 +0800

    update cli main.cpp

--- src/mvs-cli/main.cpp
+++ src/mvs-cli/main.cpp
@@ -19,10 +19,11 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #include <iostream>
+#include <json/minijson_writer.hpp>
 #include <metaverse/explorer.hpp>
 #include <metaverse/mgbubble/MongooseCli.hpp>
-#include <json/minijson_writer.hpp>
 #include <metaverse/explorer/extensions/command_extension_func.hpp>
+#include <metaverse/explorer/extensions/exception.hpp>
 
 BC_USE_MVS_MAIN
 
@@ -58,12 +59,31 @@ int bc::main(int argc, char* argv[])
         return std::memcmp(cmd, "fetch-", 6) == 0;
     };
 
-    if (!cmd_ptr && !is_online_cmd(cmd.c_str())){
-        auto ret = bc::explorer::dispatch_command(argc - 1, 
-            const_cast<const char**>(argv + 1), bc::cin, bc::cout, bc::cerr);
-        bc::cout<<std::endl;
-        return ret;
-    }
+	do{
+		std::stringstream sout;
+		console_result ret;
+		try {
+			if (!cmd_ptr && !is_online_cmd(cmd.c_str())) {
+				ret = bc::explorer::dispatch_command(argc - 1,
+					const_cast<const char**>(argv + 1), bc::cin, sout, bc::cerr);
+				if (ret != console_result::okay) {
+					throw explorer::command_params_exception(sout.str());
+				}
+				std::pair<uint32_t, std::string> ex_pair;
+				std::stringstream ex_stream;
+				ex_stream.str(sout.str());
+				if (explorer::capture_excode(ex_stream, ex_pair) == console_result::okay) {
+					throw explorer::explorer_exception(ex_pair.first, ex_pair.second);
+				}
+			}
+		}
+		catch (bc::explorer::explorer_exception e) {
+			sout << e;
+		}
+		bc::cout << sout.str() << std::endl;
+		return ret;
+	} while (0);
+
 
 	// extension commands
     HttpReq req("127.0.0.1:8820/rpc", 3000, reply_handler(my_impl));
