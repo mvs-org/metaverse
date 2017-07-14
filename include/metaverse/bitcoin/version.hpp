a0d55c472afd7575c4c21f2f0226fd7ad951904c
ydxt25 <928364432@qq.com>
2017-07-14 17:12:15 +0800

    v0.6.10

--- include/metaverse/bitcoin/version.hpp
+++ include/metaverse/bitcoin/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define MVS_VERSION "0.6.9"
+#define MVS_VERSION "0.6.10"
 #define MVS_MAJOR_VERSION 0
 #define MVS_MINOR_VERSION 6
-#define MVS_PATCH_VERSION 8
+#define MVS_PATCH_VERSION 10
 
 #endif
