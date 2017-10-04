8a99c44e82006a201c41f5d56602b78288e24a30
sikamedia <simonwantao@yahoo.com>
2017-10-04 22:18:20 +0200

    Only return :  height 227361, not {"height": 227361} https://github.com/mvs-org/metaverse/issues/137

--- include/metaverse/explorer/prop_tree.hpp
+++ include/metaverse/explorer/prop_tree.hpp
@@ -409,7 +409,7 @@ BCX_API pt::ptree prop_tree(const wallet::bitcoin_uri& uri);
 
 BCX_API pt::ptree prop_tree(const block& block);
 
-BCX_API std::string prop_tree(const size_t height);
+BCX_API pt::ptree prop_tree(const size_t height);
 
 } // namespace config
 } // namespace explorer
