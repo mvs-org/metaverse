7847a8f2f479a1985edb830e086a7b183515d602
betachen <betachen@yeah.net>
2017-12-06 17:08:00 +0800

    re-implement getnewaccount and importaccount

--- src/lib/explorer/extensions/commands/importaccount.cpp
+++ src/lib/explorer/extensions/commands/importaccount.cpp
@@ -32,6 +32,7 @@
 #include <metaverse/explorer/extensions/command_extension_func.hpp>
 #include <metaverse/explorer/extensions/command_assistant.hpp>
 #include <metaverse/explorer/extensions/exception.hpp> 
+#include <metaverse/explorer/commands/offline_commands_impl.hpp> 
 
 namespace libbitcoin {
 namespace explorer {
@@ -42,74 +43,24 @@ namespace pt = boost::property_tree;
 console_result importaccount::invoke (std::ostream& output,
         std::ostream& cerr, libbitcoin::server::server_node& node)
 {
-    std::istringstream sin("");
-    std::ostringstream lang("");
-    std::stringstream sout("");
     
     // parameter account name check
     auto& blockchain = node.chain_impl();
     if (blockchain.is_account_exist(auth_.name))
      throw account_existed_exception{"account already exist"};
 
+#ifdef NDEBUG
 	if (auth_.name.length() > 128 || auth_.name.length() < 3 ||
-		option_.passwd.length() > 128 || option_.passwd.length() < 6)
-		throw argument_exceed_limit_exception{"name length in [3, 128], password length in [6, 128]"};
-    
-    if (argument_.words.size() > 24)
-        throw argument_exceed_limit_exception{"word count must be less than or equal 24"};
-    
-    for(auto& i : argument_.words){
-        sout<<i<<" ";
-    }
-    
-    auto mnemonic = sout.str().substr(0, sout.str().size()-1); // remove last space
-    
-    // 1. parameter mnemonic check
-    lang<<option_.language;
-    sin.str("");
-    sout.str("");
+	    option_.passwd.length() > 128 || option_.passwd.length() < 6)
+	    throw argument_exceed_limit_exception{"name length in [3, 128], password length in [6, 128]"};
+#endif
 
-    //const char* cmds[256]{"mnemonic-to-seed", "-l", lang.str().c_str()};
-    const char* cmds[64]{0x00};
-    int i = 0;
-    cmds[i++] = "mnemonic-to-seed";
-    cmds[i++] = "-l";
-    auto s_lang = lang.str();
-    cmds[i++] = s_lang.c_str();
-    // words size check
-    std::vector<std::string> tokens;
-    if(argument_.words.size() == 24) {
-        for(auto& word : argument_.words){
-            cmds[i++] = word.c_str();
-        }
-    } else if(argument_.words.size() == 1) { // all words include in ""
-        tokens = split(argument_.words.at(0));
-        for(auto& word : tokens){
-            cmds[i++] = word.c_str();
-        }
-    } else {
-        throw argument_size_invalid_exception{"words count should be 24, not " + std::to_string(argument_.words.size())};
-    }
+    // are vliad mnemonic words.
+    auto&& seed = get_mnemonic_to_seed(option_.language, argument_.words);
+    // is vliad seed.
+    auto&& hd_pri_key = get_hd_new(seed);
 
-    if(dispatch_command(i, cmds , sin, sout, sout) != console_result::okay) {
-        throw mnemonicwords_to_seed_exception(sout.str());
-    }
-     
-     
-    relay_exception(sout);
-    // 2. check mnemonic exist in account database
-    #if 0 // mnemonic is encrypted by passwd so no check now
-    auto is_mnemonic_exist = false;
-    auto sh_vec = blockchain.get_accounts();
-    for(auto& each : *sh_vec) {
-        if(mnemonic == each.get_mnemonic()){
-            is_mnemonic_exist = true;
-            break;
-        }
-    }
-    if(is_mnemonic_exist)
-        throw mnemonicwords_existed_exception{"mnemonic already exist!"};
-    #endif
+    auto&& mnemonic = bc::join(argument_.words);
 
     // create account
     auto acc = std::make_shared<bc::chain::account>();
@@ -117,6 +68,7 @@ console_result importaccount::invoke (std::ostream& output,
     acc->set_passwd(option_.passwd);
     acc->set_mnemonic(mnemonic, option_.passwd);
     //acc->set_hd_index(option_.hd_index); // hd_index updated in getnewaddress
+    
     // flush to db
     blockchain.store_account(acc);
 
@@ -129,6 +81,8 @@ console_result importaccount::invoke (std::ostream& output,
     uint32_t idx = 0;
     const char* cmds2[]{"getnewaddress", auth_.name.c_str(), option_.passwd.c_str()};
     pt::ptree addresses;
+    std::istringstream sin("");
+    std::stringstream sout("");
     
     for( idx = 0; idx < option_.hd_index; idx++ ) {
         pt::ptree addr;
@@ -144,7 +98,6 @@ console_result importaccount::invoke (std::ostream& output,
     }
 
     root.add_child("addresses", addresses);
-    
     pt::write_json(output, root);
     
     return console_result::okay;
