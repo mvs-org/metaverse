d5350d01b724b66d715a60097e5378acfe5b0627
betachen <betachen@yeah.net>
2017-12-11 22:16:53 +0800

    add getheight

---
+++ src/lib/explorer/extensions/commands/getheight.cpp
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2016-2017 mvs developers 
+ *
+ * This file is part of metaverse-explorer.
+ *
+ * metaverse-explorer is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <jsoncpp/json/json.h>
+#include <metaverse/explorer/extensions/commands/getheight.hpp>
+#include <metaverse/explorer/extensions/command_extension_func.hpp>
+#include <metaverse/explorer/extensions/exception.hpp>
+
+namespace libbitcoin {
+namespace explorer {
+namespace commands {
+
+/************************ getheight *************************/
+
+console_result getheight::invoke (std::ostream& output,
+        std::ostream& cerr, libbitcoin::server::server_node& node)
+{
+    administrator_required_checker(node, auth_.name, auth_.auth);
+
+    Json::Value jv; 
+    jv["height"] = get_last_height(node);
+
+    output << jv.toStyledString();
+
+    return console_result::okay;
+}
+
+
+} // namespace commands
+} // namespace explorer
+} // namespace libbitcoin
+
