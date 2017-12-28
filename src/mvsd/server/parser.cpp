9b160793a42cd99b5af8b1ed2dd656ca6736a8e8
betachen <betachen@yeah.net>
2017-12-28 12:17:12 +0800

    remove double setting for mongoose

--- src/mvsd/server/parser.cpp
+++ src/mvsd/server/parser.cpp
@@ -325,24 +325,19 @@ options_metadata parser::load_settings()
 
     /* [server] */
 	(
-		"server.mongoose_listen",
-		value<std::string>(&configured.server.mongoose_listen),
-		"The Json-RPC listening port, defaults to 127.0.0.1:8820."
-	)
-	(
-		"server.administrator_required",
-		value<bool>(&configured.server.administrator_required),
-		"Whether wallet needs administrator to execute non-account commands(shutdown/getinfo...), defaults to false."
+	    "server.administrator_required",
+	    value<bool>(&configured.server.administrator_required),
+	    "Whether wallet needs administrator to execute non-account commands(shutdown/getinfo...), defaults to false."
 	)
     (
         "server.mongoose_listen",
         value<std::string>(&configured.server.mongoose_listen),
-        "the listening port for mongoose, defaults to 127.0.0.1:8820."
+        "The listening port for mongoose(Json-RPC), defaults to 127.0.0.1:8820."
     )
     (
         "server.websocket_listen",
         value<std::string>(&configured.server.websocket_listen),
-        "the listening port for websocket pub/sub service, defaults to 127.0.0.1:8821."
+        "The listening port for websocket pub/sub service, defaults to 127.0.0.1:8821."
     )
     (
         "server.query_workers",
