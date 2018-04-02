4a10e695b9a8c5d29d85af2e4e0a87edcefd5698
杨光路 <yangguanglu@MacBook-Pro-3.local>
2018-04-02 11:02:15 +0800

    list dids bug fix

--- src/lib/explorer/extensions/commands/listdids.cpp
+++ src/lib/explorer/extensions/commands/listdids.cpp
@@ -42,12 +42,13 @@ console_result listdids::invoke (Json::Value& jv_output,
     std::string symbol;
     auto& blockchain = node.chain_impl();
     auto sh_vec = std::make_shared<std::vector<did_detail>>();
+
+    sh_vec = blockchain.get_issued_dids();
+        
+    if (sh_vec->size() == 0) // no did found
+        throw did_symbol_notfound_exception{"No did found, please waiting for block synchronizing finish."};
     
     if(auth_.name.empty()) { // no account -- list whole dids in blockchain
-        sh_vec = blockchain.get_issued_dids();
-        
-        if (sh_vec->size() == 0) // no did found
-            throw did_symbol_notfound_exception{"No did found, please waiting for block synchronizing finish."};
 
         // add blockchain dids
         for (auto& elem: *sh_vec) {
@@ -68,12 +69,23 @@ console_result listdids::invoke (Json::Value& jv_output,
             throw address_list_nullptr_exception{"nullptr for address list"};
         
         for (auto& elem: *sh_vec) {
+
+            auto address = elem.get_address();            
+            auto pos = std::find_if(pvaddr->begin(), pvaddr->end(), [&](const account_address& elem){
+                    return address == elem.get_address();
+                    });
+            
+            if (pos == pvaddr->end()){ // did is not yours
+                continue;
+            } 
+
             Json::Value did_data;
             did_data["symbol"] = elem.get_symbol();
             symbol = elem.get_symbol();
             did_data["issuer"] = elem.get_issuer();
+            did_data["address"] = elem.get_address();
             did_data["description"] = elem.get_description();
-            did_data["status"] = "unspent";
+            did_data["status"] = "issued";
             dids.append(did_data);
         }
         // 2. get did in local database
