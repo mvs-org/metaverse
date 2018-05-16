e390c33d0508d5b8df9c3d1e01250178d10d5132
chengzhiping <chengzhpchn@163.com>
2018-05-16 19:40:10 +0800

    test case for did

--- test/test-rpc/main.py
+++ test/test-rpc/main.py
@@ -5,14 +5,13 @@ import TestCase
 
 def run_testcase():
     ret = []
+    with open('mvs_test_report.txt', 'w') as f:
+        category_lst = ['Account', "Blockchain", "Block", "Identity", "ETP", "Asset", "MultiSignature", "RawTx", "Transaction"]
+        for i in category_lst:
+            test_dir = "./TestCase/" + i
+            print >> f, '====', i, '===='
+            discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_*.py', top_level_dir="./TestCase/")
 
-    category_lst = ['Account', "Blockchain", "Block", "Identity", "ETP", "Asset", "MultiSignature", "RawTx", "Transaction"]
-    for i in category_lst:
-        test_dir = "./TestCase/" + i
-        print i
-        discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_*.py', top_level_dir="./TestCase/")
-
-        with open('%s_test_report.txt' % i, 'w') as f:
             runner = unittest.TextTestRunner(stream=f, verbosity=2)
             runner.run(discover)
 
