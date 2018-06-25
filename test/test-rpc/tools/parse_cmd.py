77de5ffa745c5515e2e69c8865b9c93ef2c1fd29
chengzhiping <chengzhpchn@163.com>
2018-06-25 10:14:23 +0800

    add remarks for transaciton and store them in local database

--- test/test-rpc/tools/parse_cmd.py
+++ test/test-rpc/tools/parse_cmd.py
@@ -3,6 +3,85 @@ import re
 
 cmd_dir = '/home/czp/GitHub/nova/metaverse/include/metaverse/explorer/extensions/commands'
 
+rettypes = '''
+getblockheader, blockheader
+getblock, block
+getnewaccount,account
+getnewaddress,List<String>
+listaddresses,List<String>
+validateaddress,address
+importaccount,accountEx
+importkeyfile,String
+dumpkeyfile,String
+changepasswd,accountstatus
+deleteaccount,accountstatus
+getaccount,accountresult
+shutdown,String
+getinfo,info
+getheight,UInt64
+getpeerinfo,List<String>
+getmininginfo,mining_info
+startmining,String
+stopmining,String
+getwork,List<String>
+addnode,String
+submitwork,Boolean
+getmemorypool,List<transaction>
+getbalance,balances
+listbalances,List<balance>
+deposit,transaction
+send,transaction
+sendfrom,transaction
+sendmore,transaction
+gettx,transaction
+listtxs,transactions
+createasset,asset
+deletelocalasset,delasset
+issue,transaction
+getaccountasset,List<asset>
+getaddressasset,List<asset>
+getasset,List<String>
+listassets,List<asset>
+sendasset,transaction
+sendassetfrom,transaction
+secondaryissue,transaction
+issuecert,transaction
+transfercert,transaction
+burn,transaction
+getnewmultisig,mulsignature
+listmultisig,List<mulsignature>
+deletemultisig,mulsignature
+createmultisigtx,String
+signmultisigtx,String
+createrawtx,String
+signrawtx,rawtx
+decoderawtx,transaction
+sendrawtx,String
+registerdid,transaction
+listdids,List<did>
+didsend,transaction
+didsendmore,transaction
+didsendfrom,transaction
+didsendasset,transaction
+didsendassetfrom,transaction
+didchangeaddress,transaction
+getdid,List<didhistoryaddress>
+registermit,transaction
+transfermit,transaction
+listmits,List<mit>
+getmit,List<mit>
+getpublickey,String
+popblock,String
+fetchheaderext,String
+getaddressetp,String
+setminingaccount,String
+'''
+
+func2type = {}
+for i in rettypes.strip().split('\n'):
+    k,v = i.split(',')
+    func2type[k.strip()] = v.strip()
+
 def get_paraname(line):
     pattern = '"([/\w]+)(,\w+){0,1}"'
     m = re.search(pattern, line)
@@ -118,6 +197,7 @@ def %(cmdname)s(%(argument_defines)s):
 %(comments)s
     \'\'\'
     positional=[%(positional_arguments)s]
+    %(token_arguments)s
     optional={
         %(optional_arguments)s
     }
@@ -157,10 +237,14 @@ def %(cmdname)s(%(argument_defines)s):
     argument_defines_2 = [] #  with default value
     optional_arguments = []
     positional_arguments = []
+    token_arguments = []
     for param_name, param_def, param_desc in params:
         comments.append("    :param: %s(%s): %s" % (param_name, cpp2py_type(param_def["para_type"]), param_desc))
         if param_def.get("must_give", False) == False:
-            argument_defines_2.append('%s=None' % param_name)
+            if "colon_delimited2_item" in param_def["para_type"]:
+                argument_defines_2.append('%s=(0,0)' % param_name)
+            else:
+                argument_defines_2.append('%s=None' % param_name)
         else:
             argument_defines_1.append('%s' % param_name)
 
@@ -169,6 +253,11 @@ def %(cmdname)s(%(argument_defines)s):
                 positional_arguments.append("' '.join(%s)" % param_name)
             else:
                 positional_arguments.append(param_name)
+            continue
+
+        if param_def.get("just_token", False) == True:
+            assert param_def["para_type"] == "bool"
+            token_arguments.append(param_name)
             continue
 
         if "colon_delimited2_item" in param_def["para_type"]:
@@ -184,6 +273,7 @@ def %(cmdname)s(%(argument_defines)s):
         'argument_defines': ', '.join(argument_defines_1 + argument_defines_2),
         'optional_arguments': '\n        '.join(optional_arguments),
         'positional_arguments': ', '.join(positional_arguments),
+        'token_arguments': '\n    '.join('if %s == True: positional.append("--%s")' % (i, i) for i in token_arguments)
     }
 
     print template % paras
@@ -281,31 +371,32 @@ def generate_DotNet_SDK(cmdname, pos_params, params):
 /*
 %(comments)s
 */
-public String %(func_name)s(%(argument_defines)s)
+public %(rettype)s %(func_name)s(%(argument_defines)s)
 {
     List<String> parameters = new List<String>() { %(positional_arguments)s };
+    %(token_arguments)s
     %(add_optional_parameters)s
-    return getResult<String>("%(func_name)s", parameters);
+    return getResult<%(rettype)s>("%(func_name)s", parameters);
 }
 '''
     def cpp2dotnet_type(cpp_type):
         convert = {
-            'uint8_t' : 'UInt8',
-            'uint16_t': 'UInt16',
-            'uint32_t': 'UInt32',
-            'std::uint32_t' : 'UInt32',
-            'uint64_t': 'UInt64',
+            'uint8_t' : 'UInt8?',
+            'uint16_t': 'UInt16?',
+            'uint32_t': 'UInt32?',
+            'std::uint32_t' : 'UInt32?',
+            'uint64_t': 'UInt64?',
 
-            'int8_t': 'Int8',
-            'int16_t': 'Int16',
-            'int32_t': 'Int32',
-            'std::int32_t': 'Int32',
-            'int64_t': 'Int64',
+            'int8_t': 'Int8?',
+            'int16_t': 'Int16?',
+            'int32_t': 'Int32?',
+            'std::int32_t': 'Int32?',
+            'int64_t': 'Int64?',
 
-            'non_negative_uint64' : 'UInt64',
+            'non_negative_uint64' : 'UInt64?',
 
             'std::string' : 'String',
-            'bool': 'Boolean',
+            'bool': 'Boolean?',
             'explorer::config::transaction' : 'String',
             'std::vector<std::string>' : 'List<String>',
             'explorer::config::language' : 'String',
@@ -329,12 +420,18 @@ public String %(func_name)s(%(argument_defines)s)
 
         return convert.get(cpp_type, cpp_type)
     comments = []
-    argument_defines = []
+    argument_defines_1 = [] # with no default valie
+    argument_defines_2 = [] #  with default value
     optional_arguments = []
     positional_arguments = []
+    token_arguments = []
     for param_name, param_def, param_desc in params:
         comments.append("    :param: %s(%s): %s" % (param_name, special_type_desc(param_def["para_type"]), param_desc))
-        argument_defines.append('%s %s' % (cpp2dotnet_type(param_def["para_type"]), param_name))
+        #argument_defines.append('%s %s' % (cpp2dotnet_type(param_def["para_type"]), param_name))
+        if param_def.get("must_give", False) == False:
+            argument_defines_2.append('%s %s=null' % (cpp2dotnet_type(param_def["para_type"]), param_name))
+        else:
+            argument_defines_1.append('%s %s' %  (cpp2dotnet_type(param_def["para_type"]).replace('?', ''), param_name) )
 
         if param_name in pos_params:
             if cpp2dotnet_type(param_def["para_type"]) == 'String':
@@ -344,23 +441,35 @@ public String %(func_name)s(%(argument_defines)s)
             else:
                 positional_arguments.append('%s.ToString()' % param_name)
             continue
+
+        if param_def.get("just_token", False) == True:
+            assert param_def["para_type"] == "bool"
+            token_arguments.append(param_name)
+            continue
+
         if "vector<" in param_def["para_type"]:
             optional_arguments.append('foreach (var i in %s) {' % param_name)
             optional_arguments.append('    parameters.AddRange(new List<String>{"--%s", i.ToString()});' % param_name)
             optional_arguments.append('}')
         else:
+            prefix = ""
+            if param_def.get("must_give", False) == False:
+                prefix = "if (%s != null) " % param_name
+
             if "colon_delimited2_item" in param_def["para_type"]:
                 value = 'parameters.AddRange(new List<String>{"--%s", String.Format("{0}:{1}", %s.Item1, %s.Item2)});' % (param_name, param_name, param_name)
             else:
                 value = 'parameters.AddRange(new List<String>{"--%s", %s.ToString()});' % (param_name, param_name)
-            optional_arguments.append(value)
+            optional_arguments.append(prefix + value)
 
     paras = {
         'comments': '\n'.join(comments),
         'func_name': cmdname,
-        'argument_defines': ', '.join(argument_defines),
+        'argument_defines': ', '.join(argument_defines_1 + argument_defines_2),
         'add_optional_parameters': '\n    '.join(optional_arguments),
         'positional_arguments': ', '.join(positional_arguments),
+        'rettype' : func2type[cmdname],
+        'token_arguments': '\n    '.join([ 'if (%s == true) parameters.Add("--%s");' % (i, i) for i in token_arguments ])
     }
 
     print template % paras
