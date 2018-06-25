830b4a5c503dea23557ba426eb5dc594e021c826
luozhaohui <zhaohui.luo@viewfin.com>
2018-06-25 16:55:55 +0800

    update version to 0.8.2.

--- include/metaverse/explorer/version.hpp
+++ include/metaverse/explorer/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define MVS_EXPLORER_VERSION "0.8.0"
+#define MVS_EXPLORER_VERSION "0.8.2"
 #define MVS_EXPLORER_MAJOR_VERSION 0
 #define MVS_EXPLORER_MINOR_VERSION 8
-#define MVS_EXPLORER_PATCH_VERSION 0
+#define MVS_EXPLORER_PATCH_VERSION 2
 
 #endif
