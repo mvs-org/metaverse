99d087935a49f84bd267c8622b15abf1b2ecfcf8
Jowen <jowen.xiao@viewfin.com>
2018-09-29 10:17:18 +0800

    modify version to 0.8.5

--- include/metaverse/explorer/version.hpp
+++ include/metaverse/explorer/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define MVS_EXPLORER_VERSION "0.8.4"
+#define MVS_EXPLORER_VERSION "0.8.5"
 #define MVS_EXPLORER_MAJOR_VERSION 0
 #define MVS_EXPLORER_MINOR_VERSION 8
-#define MVS_EXPLORER_PATCH_VERSION 4
+#define MVS_EXPLORER_PATCH_VERSION 5
 
 #endif
