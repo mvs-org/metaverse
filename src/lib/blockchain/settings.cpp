85d739509a6cf0ff529091c811668869ddd47918
jowenshaw <xiaozongyuan@163.com>
2019-01-18 21:51:23 +0800

    add unstrict synced block information

--- src/lib/blockchain/settings.cpp
+++ src/lib/blockchain/settings.cpp
@@ -35,7 +35,9 @@ settings::settings()
     transaction_pool_capacity(4096),
     transaction_pool_consistency(false),
     use_testnet_rules(false),
-    collect_split_stake(true)
+    collect_split_stake(true),
+    checkpoints(),
+    basic_checkpoints()
 {
 }
 
@@ -50,23 +52,18 @@ settings::settings(bc::settings context)
         {
             // FIXEME.p2p-node.cpp, This is for header-first sync, attach_header_sync_session
             // header sync first has some problem.
-            //basic_checkpoints.reserve(1);
-            //basic_checkpoints.push_back({ "b81848ef9ae86e84c3da26564bc6ab3a79efc628239d11471ab5cd25c0684c2d", 0 });
+            basic_checkpoints.reserve(1);
+            basic_checkpoints.push_back({ "b81848ef9ae86e84c3da26564bc6ab3a79efc628239d11471ab5cd25c0684c2d", 0 });
 
-            // validate_block, This is for each singel node validate
-            checkpoints.reserve(1);
+            // validate_block, This is for each single node validate
+            checkpoints.reserve(7);
             checkpoints.push_back({ "b81848ef9ae86e84c3da26564bc6ab3a79efc628239d11471ab5cd25c0684c2d", 0 });
-            //checkpoints.push_back({ "d4f84eb9acb52cbfb003bd4fb88f4b5cf1f6328f193097d42b9403d7030abb8e", 3800 });
-            //checkpoints.push_back({ "e989e4b2d60ae2f8fbaa1cdb69d05afa63e7f1f99cf715589a93e4877c92fa8b", 100000 });
-            //checkpoints.push_back({ "d120edf7e14d8f426b7390e94ae7bfd746f9fc247b28e336db052295177ac9d3", 410000 }); //0.6.9 fix
-            //checkpoints.push_back({ "9a0efd7b41cfc1cbeb1bfbd2ab3cb7989314611608cc4236b80a540444fbfb36", 800000 });
-            //checkpoints.push_back({ "ed11a074ce80cbf82b5724bea0d74319dc6f180198fa1bbfb562bcbd50089e63", 1030000 }); //future time attack
-            //checkpoints.push_back({ "a18a5aa5270835dd720a561c20230435e0508e8339ee30998da6dd79eee83b29", 1270000 }); //super nova
-
-#ifdef PRIVATE_CHAIN
-            checkpoints.clear();
-            checkpoints.push_back({ "b81848ef9ae86e84c3da26564bc6ab3a79efc628239d11471ab5cd25c0684c2d", 0 });
-#endif
+            checkpoints.push_back({ "d4f84eb9acb52cbfb003bd4fb88f4b5cf1f6328f193097d42b9403d7030abb8e", 3800 });
+            checkpoints.push_back({ "e989e4b2d60ae2f8fbaa1cdb69d05afa63e7f1f99cf715589a93e4877c92fa8b", 100000 });
+            checkpoints.push_back({ "d120edf7e14d8f426b7390e94ae7bfd746f9fc247b28e336db052295177ac9d3", 410000 }); //0.6.9 fix
+            checkpoints.push_back({ "9a0efd7b41cfc1cbeb1bfbd2ab3cb7989314611608cc4236b80a540444fbfb36", 800000 });
+            checkpoints.push_back({ "ed11a074ce80cbf82b5724bea0d74319dc6f180198fa1bbfb562bcbd50089e63", 1030000 }); //future time attack
+            checkpoints.push_back({ "a18a5aa5270835dd720a561c20230435e0508e8339ee30998da6dd79eee83b29", 1270000 }); //super nova
 
             bc::wallet::ec_private::mainnet_p2kh = 0x32;
             bc::wallet::ec_public::mainnet_p2kh = 0x32;
@@ -78,8 +75,9 @@ settings::settings(bc::settings context)
         {
             use_testnet_rules = true;
 
-            //basic_checkpoints.reserve(1);
-            //basic_checkpoints.push_back({ "b1076144f919c8efaf55e5ec267daa6d5dc0a12609c9c6fddf8157270ae6e7ca", 0 });
+            basic_checkpoints.reserve(1);
+            basic_checkpoints.push_back({ "c359a1cc3dfb8b97111c3e602f1f6de31306926f9ec779cb9ea002edbee91741", 0 });
+
             checkpoints.reserve(1);
             checkpoints.push_back({ "c359a1cc3dfb8b97111c3e602f1f6de31306926f9ec779cb9ea002edbee91741", 0 });
 
@@ -101,6 +99,17 @@ settings::settings(bc::settings context)
         {
         }
     }
+
+#ifdef PRIVATE_CHAIN
+    {
+        basic_checkpoints.clear();
+        basic_checkpoints.push_back({ "b694bd9fae0fec221e24bde774d5414df994ed536909c4ffca49d55d0f14f40f", 0 });
+
+        checkpoints.clear();
+        checkpoints.push_back({ "b694bd9fae0fec221e24bde774d5414df994ed536909c4ffca49d55d0f14f40f", 0 });
+    }
+#endif
+
 }
 
 } // namespace blockchain
